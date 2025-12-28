f20744bf88ceb3f848840dbfc7689c24bf19dd4a
Maxey1950 <jfspector3@gmail.com>
2025-12-28 13:16:18 -0500

    Auto-update offsets

--- dumped_offsets.hpp
+++ dumped_offsets.hpp
@@ -1,19 +1,17 @@
 /*
-    PID: 47400
-    DUMPER BY VOLXPHY
+    PID: 49760
+    injector offset adder BY maxey
     REPO: https://github.com/fkenedddyjon-png/robloxoffsets
 */
 
 // INTERNAL OFFSETS
 inline uintptr_t RawTaskScheduler = REBASE(0x7E1CB88);
-inline uintptr_t FakeDataModelPointer = REBASE(0x7D03628);
 
 // EXTERNAL OFFSETS
 inline uintptr_t Overlap = 0x288;
 inline uintptr_t GlobalState = 0x140;
 inline uintptr_t RequireBypass = 0x920;
 inline uintptr_t GetSetImplementation = 0x90;
-inline uintptr_t ClassDescriptor = 0x18;
 
 // HYPERION OFFSETS
 inline uintptr_t BitMap = REBASEHYPERION(0x51F18);
