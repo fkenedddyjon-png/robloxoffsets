3fafadf427d639e487c5bc51712223592eab4adf
Maxey1950 <jfspector3@gmail.com>
2025-12-31 13:38:17 -0500

    Auto-update offsets

--- dumped_offsets.hpp
+++ dumped_offsets.hpp
@@ -1,17 +1,16 @@
 /*
-    PID: 32796
+    PID: 29556
     injector offset adder BY maxey
 */
 
 // INTERNAL OFFSETS
 inline uintptr_t RawTaskScheduler = REBASE(0x7E1CB88);
-inline uintptr_t FakeDataModelPointer = REBASE(0x7D03628);
 
 // EXTERNAL OFFSETS
 inline uintptr_t Overlap = 0x288;
 inline uintptr_t GlobalState = 0x140;
-inline uintptr_t RequireBypass = 0x0; // FAILED TO DUMP
-inline uintptr_t ClassDescriptor = 0x18;
+inline uintptr_t RequireBypass = 0x920;
+inline uintptr_t GetSetImplementation = 0x90;
 
 // HYPERION OFFSETS
 inline uintptr_t BitMap = REBASEHYPERION(0x11658F8);
