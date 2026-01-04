a8883541853a7881ec103358a85340b0188f8acf
Maxey1950 <jfspector3@gmail.com>
2026-01-04 18:04:50 -0500

    Auto-update offsets

--- dumped_offsets.hpp
+++ dumped_offsets.hpp
@@ -1,28 +1,16 @@
 /*
-    PID: 46536
+    PID: 40636
     injector offset adder BY maxey
 */
 
 // INTERNAL OFFSETS
 inline uintptr_t RawTaskScheduler = REBASE(0x7E1CB88);
-inline uintptr_t FakeDataModelPointer = REBASE(0x7D03628);
 
 // EXTERNAL OFFSETS
 inline uintptr_t Overlap = 0x288;
 inline uintptr_t GlobalState = 0x140;
+inline uintptr_t RequireBypass = 0x920;
 inline uintptr_t GetSetImplementation = 0x90;
-inline uintptr_t Name = 0xB0;
-inline uintptr_t GameLoaded = 0x600;
-inline uintptr_t Children = 0x70;
-inline uintptr_t Workspace = 0x178;
-inline uintptr_t ClassDescriptor = 0x18;
-inline uintptr_t Camera = 0x450;
-inline uintptr_t LocalPlayer = 0x130;
-inline uintptr_t JumpPower = 0x1B0;
-inline uintptr_t Walkspeed = 0x1D4;
-inline uintptr_t WalkspeedCheck = 0x3C0;
-inline uintptr_t Health = 0x194;
-inline uintptr_t ScriptContext = 0x3F0;
 
 // HYPERION OFFSETS
 inline uintptr_t BitMap = REBASEHYPERION(0x11658F8);
