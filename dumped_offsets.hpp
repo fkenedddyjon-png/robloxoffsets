6d846940be6949f6f40b70c9dfe3188a70398a73
Maxey1950 <jfspector3@gmail.com>
2026-01-14 19:25:15 -0500

    Auto-update offsets

--- dumped_offsets.hpp
+++ dumped_offsets.hpp
@@ -1,20 +1,20 @@
 /*
-    PID: 23604
+    PID: 33952
     injector offset adder BY maxey
 */
 
 // INTERNAL OFFSETS
-inline uintptr_t RawTaskScheduler = REBASE(0x81219C8);
-inline uintptr_t FakeDataModelPointer = REBASE(0x8006F88);
+inline uintptr_t RawTaskScheduler = REBASE(0x8119EC8);
+inline uintptr_t FakeDataModelPointer = REBASE(0x7FF0818);
 
 // EXTERNAL OFFSETS
 inline uintptr_t Overlap = 0x288;
 inline uintptr_t GlobalState = 0x140;
-inline uintptr_t RequireBypass = 0x0; // FAILED TO DUMP
+inline uintptr_t RequireBypass = 0x964;
 inline uintptr_t GetSetImplementation = 0x90;
 inline uintptr_t ClassDescriptor = 0x18;
 
 // HYPERION OFFSETS
-inline uintptr_t BitMap = REBASEHYPERION(0xF30408);
-inline uintptr_t ControlFlowGuard = REBASEHYPERION(0xEFD210);
-inline uintptr_t NtUnmapViewOfSectionSyscall = REBASEHYPERION(0x89B99C);
+inline uintptr_t BitMap = REBASEHYPERION(0x1C55C8);
+inline uintptr_t ControlFlowGuard = REBASEHYPERION(0xD3ADF0);
+inline uintptr_t NtUnmapViewOfSectionSyscall = REBASEHYPERION(0x5F08E4);
